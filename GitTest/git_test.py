fea9da159fc8d8e167a3da88f613b37b5674e738
AdrianW10 <159167587+AdrianW10@users.noreply.github.com>
2024-11-23 20:27:14 +0100

    Test Programm
    Test Programm in dem jeder Änderungen vornehmen soll und diese dann in einer eigenen branch pushen soll.

---
+++ GitTest/git_test.py
@@ -0,0 +1,40 @@
+##############
+###Stefan#####
+##############
+
+"""
+Ergänze den folgenden code, sodass Quersumme richtig ausgegeben wird:
+"""
+def qsum(n):
+    s = 0
+    while n > 9:
+        """
+        füge den code ein
+        """
+    s += n
+    print(s)
+
+##############
+###Alina######
+##############
+
+"""
+Erstelle eine Funktion deiner Wahl im folgenden Abschnitt:
+"""
+
+def alinas_function():
+    ...
+
+##############
+###Laurin#####
+##############
+
+"""
+Mache irgednwas mit der folgenden Matrix:
+"""
+
+zero_matrix = [[0 for i in range(10)] for j in range(10)]
+
+for i in range(10):
+    print(zero_matrix[i])
+
